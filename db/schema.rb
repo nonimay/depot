bdfc5f68a55a73ad39ba9e8a8973f9387d285eae
Fiona McDonald <fiona.m.mcdonald@gmail.com>
2011-07-01 20:16:34 +0100

    TaskF

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110701130700) do
+ActiveRecord::Schema.define(:version => 20110701164611) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(:version => 20110701130700) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   :default => 1
+    t.decimal  "price"
   end
 
   create_table "products", :force => true do |t|
